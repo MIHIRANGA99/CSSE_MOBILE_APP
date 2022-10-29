5bbeafd95a67b2a3fd736b487c65247a47b826c6
MIHIRANGA99 <kavisha.mihiranga@yahoo.com>
2022-10-29 07:42:33 +0530

    project (50% done)

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import HomeScreen from './screens/HomeScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import LoginScreen from './screens/LoginScreen';
+import Dashboard from './screens/Dashboard';
+import NewOrderScreen from './screens/SiteManager/NewOrderScreen';
+import AllOrdersScreen from './screens/SiteManager/AllOrdersScreen';
+import Quotations from './screens/Supplier/Quotations';
+import NewInvoiceScreen from './screens/Supplier/NewInvoiceScreen';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Register'>
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Register' component={RegisterScreen} />
+        <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen name='Dashboard' component={Dashboard} />
+        <Stack.Screen name='NewOrder' component={NewOrderScreen} />
+        <Stack.Screen name='AllOrders' component={AllOrdersScreen} />
+        <Stack.Screen name='Quotes' component={Quotations} />
+        <Stack.Screen name='NewInvoice' component={NewInvoiceScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
